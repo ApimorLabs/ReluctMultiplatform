ed892d757cfae57d93bb2373a468c9a611e4342d
Racka <bmkilaha@gmail.com>
2024-08-30 11:23:03 +0300

    detekt fixes
    Signed-off-by: Racka <bmkilaha@gmail.com>

--- system-services/src/desktopMain/kotlin/work/racka/reluct/system/services/haptics/DesktopHapticFeedback.kt
+++ system-services/src/desktopMain/kotlin/work/racka/reluct/system/services/haptics/DesktopHapticFeedback.kt
@@ -4,6 +4,7 @@ package work.racka.reluct.system.services.haptics
  * This is currently not used in desktop but we still need to provide a skeleton implementation
  * This is so that we don't have any errors when we try to use HapticFeedback in commonMain code
  */
+
 internal class DesktopHapticFeedback : HapticFeedback {
     override fun tick() { /** ACTION **/ }
 
