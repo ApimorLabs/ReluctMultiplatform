c6b6527f893b453f7c038d0ea71668fd857cf1db
Racka <bmkilaha@gmail.com>
2024-09-01 19:32:11 +0300

    setup build.gradle.kts
    Signed-off-by: Racka <bmkilaha@gmail.com>

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -8,9 +8,20 @@ kotlin {
             dependencies {
                 //Projects
                 implementation(project(":data-model"))
+                implementation(project(":data-source"))
+                implementation(project(":system-services"))
 
                 // Normal deps
+                implementation(libs.coroutines.core)
+            }
+        }
 
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.koin.test)
+                implementation(libs.coroutines.test)
+                implementation(libs.turbine.test)
+                implementation(kotlin("test"))
             }
         }
     }
