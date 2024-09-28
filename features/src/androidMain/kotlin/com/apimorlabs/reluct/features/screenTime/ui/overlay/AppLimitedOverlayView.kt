6e558e987ff9f46b2fd7672b54dae06feff8564d
Racka <bmkilaha@gmail.com>
2024-09-28 15:56:00 +0300

    setup android ui for screen-time limit
    Signed-off-by: Racka <bmkilaha@gmail.com>

--- features/src/androidMain/kotlin/com/apimorlabs/reluct/features/screenTime/ui/overlay/AppLimitedOverlayView.kt
+++ features/src/androidMain/kotlin/com/apimorlabs/reluct/features/screenTime/ui/overlay/AppLimitedOverlayView.kt
@@ -2,23 +2,33 @@ package com.apimorlabs.reluct.features.screenTime.ui.overlay
 
 import android.content.Context
 import android.util.Log
+import android.view.View
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.apimorlabs.reluct.compose.ui.theme.ReluctAppTheme
+import com.apimorlabs.reluct.compose.ui.theme.Theme
+import com.apimorlabs.reluct.data.source.settings.MultiplatformSettings
+import com.apimorlabs.reluct.features.screenTime.statistics.AppScreenTimeStatsViewModel
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 internal class AppLimitedOverlayView(
     private val context: Context,
-    // private val viewModel: AppScreenTimeStatsViewModel,
-    // private val exit: (View) -> Unit
+    private val viewModel: AppScreenTimeStatsViewModel,
+    private val exit: (View) -> Unit
 ) : KoinComponent {
 
-    // private val preferences: MultiplatformSettings by inject()
+    private val preferences: MultiplatformSettings by inject()
 
     fun getView() = ComposeView(context).apply {
         setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnDetachedFromWindow)
         setContent {
             Log.e("AppLimitedOverlayView", "Overlay UI not Implemented")
-            /*val themeValue = Theme.MATERIAL_YOU.themeValue
+            val themeValue = Theme.MATERIAL_YOU.themeValue
             ReluctAppTheme(theme = themeValue) {
                 // Put Your Stuff Here
                 val composeHaptics = LocalHapticFeedback.current
@@ -31,7 +41,7 @@ internal class AppLimitedOverlayView(
                         exit(this)
                     }
                 )
-            }*/
+            }
         }
     }
 }
